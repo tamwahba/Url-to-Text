64439f6e7e1efab7ac0b1a190230b0687615634e
Tamer Wahba <tamwahba@hotmail.com>
2016-10-04 22:47:32 -0400

    play around with capture view

--- Url to Text/ViewController.swift
+++ Url to Text/ViewController.swift
@@ -11,14 +11,16 @@ import UIKit
 import CameraManager
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var cameraView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let cameraManager = CameraManager()
-        cameraManager.cameraOutputMode = .stillImage
-        cameraManager.addPreviewLayerToView(self.view)
+        cameraManager.cameraOutputMode = .videoOnly
+        cameraManager.addPreviewLayerToView(self.cameraView!)
     }
 
     override func didReceiveMemoryWarning() {
